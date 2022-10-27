f1a57cb3546dc66b4713adc929c7986ff99545a8
Keigan-Cardoza88 <cardoza.keigs@gmail.com>
2022-10-27 23:52:05 +0530

    testing without fork

--- csp_kaa_ams.py
+++ csp_kaa_ams.py
@@ -1,1 +1,20 @@
 print("Welcome to the Airport Management System")
+
+#def main_menu():
+#    print("""==============Main Menu============
+#1. Enter Admin Interface
+#2. Enter User Interface
+#3. Exit""")
+#    choice = int(input("choose operation to be performed[1/2/3] : "))
+#    while choice != '0':
+#        if choice == '1':
+#        elif choice == '2':
+#        elif choice == '3':
+#        else: exit()
+
+
+#def change_password():
+
+
+#def admin_access():
+#    admin_access = {"Username_1" : "Arnav Padwal", "Username_2" : "Arnav Rade", " "}
