99b1324478b8b6dba294cb39ce555980dd1d569e
arnavpadwal <arnav.padwal7@gmail.com>
2022-10-28 21:39:31 +0530

    typo fix in price_calc fn

--- padwal_files/ap_fn.py
+++ padwal_files/ap_fn.py
@@ -7,7 +7,7 @@ cursor = mydb.cursor()
 def price_calc():
     price_dict={"mumbai":46, "delhi":50, "kolkata":60, "chennai":70, "goa":45, "ahmedabad":38, "pune":55, "kanpur":65, "assam":75, "kerala":40}
     fare = price_dict[source.lower()] * price_dict[destination.lower()]
-    return(price)
+    return(fare)
 
 def booking_id():
     while True:
