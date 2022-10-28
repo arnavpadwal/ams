a680306f21a9b075dc4f8ec24f3e030883a351b1
arnavpadwal <arnav.padwal7@gmail.com>
2022-10-28 19:44:18 +0530

    minor changes to folder structure

--- padwal_files/ap_fn.py
+++ padwal_files/ap_fn.py
@@ -1,4 +1,8 @@
 import random
+import mysql.connector
+
+mydb=mysql.connector.connect(host='localhost', user='root', passwd='root', database='ams')
+cursor = mydb.cursor()
 
 def price_calc():
     price_dict={"mumbai":46, "delhi":50, "kolkata":60, "chennai":70, "goa":45, "ahmedabad":38, "pune":55, "kanpur":65, "assam":75, "kerala":40}
@@ -6,9 +10,12 @@ def price_calc():
     return(price)
 
 def booking_id():
-    booking_id=random.randint(247922,993784)
-    query="select Booking_ID from user"
-    result=cursor.execute(query)
-    #if booking_id in 
+    while True:
+        booking_id=random.randint(247922,993784)
+        cursor.seek(0)
+        query="select Booking_ID from bookings"
+        result=cursor.execute(query)
+        if booking_id not in result:
+            break
     return booking_id
 print(booking_id())
