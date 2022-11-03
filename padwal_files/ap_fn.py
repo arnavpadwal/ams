a97ad5d92e113e6cb2fd6935c7d8b17c4df4a94d
arnavpadwal <arnav.padwal7@gmail.com>
2022-11-03 21:42:48 +0530

    removed return statements and fixed typo

--- padwal_files/ap_fn.py
+++ padwal_files/ap_fn.py
@@ -1,16 +1,15 @@
-import random, datetime, tabulate, mysql.connector
-
-mydb=mysql.connector.connect(host='localhost', user='root', passwd='root', database='ams')
+import datetime
+import mysql.connector
+import random
+import tabulate
+mydb = mysql.connector.connect(host='localhost', user='root', passwd='root', database='ams')
 cursor = mydb.cursor()
 
 def price_calc():
     global fare
     price_dict = {"mumbai": 46, "delhi": 50, "kolkata": 60, "chennai": 70, "goa": 45,
-    "ahmedabad": 38, "pune": 55, "kanpur": 65, "assam": 75, "kerala": 40}
-    s = source.lower()
-    d = destination.lower()
-    fare = price_dict[s] * price_dict[d]
-    return fare
+                  "ahmedabad": 38, "pune": 55, "kanpur": 65, "assam": 75, "kerala": 40}
+    fare = price_dict[source.lower()] * price_dict[destination.lower()]
 
 def booking_id():
     global b_id
@@ -23,13 +22,12 @@ def booking_id():
             if b_id not in i:
                 break
         break
-    return b_id
 
 def flight_no():
-    flt_companies={"EA":"Emirate","LA":"Lufthansa","IA":"Indigo","SA":"SpiceJet"}
+    flt_companies = {"EA": "Emirate", "LA": "Lufthansa", "IA": "Indigo", "SA": "SpiceJet"}
     global flt_no
     flt_no = input("\nEnter flight number of your choice: ").upper()
-    #check flight number and allot company name accordingly
+    # check flight number and allot company name accordingly
     flt_codes = flt_companies.keys()
     for i in flt_codes:
         if i == flt_no[0:2]:
@@ -37,11 +35,10 @@ def flight_no():
             company_name = flt_companies[i]
             is_valid = True
             break
-    return flt_no
 
 def user_details():
     global phone, ticket_qty, booking_date, flight_date
-    #user input
+    # user input
     print("\nEnter the following details to proceed with the booking:\n")
     phone = int(input("\nEnter phone number: "))
     is_valid = False
@@ -60,7 +57,7 @@ def seat_no():
     result = cursor.fetchall()
     for i in result:
         x = (i[0].split(","))
-        for i in x:
+        for j in x:
             allotted_seats.append(int(i))
     for i in range(ticket_qty):
         while True:
@@ -68,10 +65,11 @@ def seat_no():
             if s_no not in allotted_seats:
                 break
             break
-    return s_no
 
-def user_invoice(): #USE TABULATE HERE
+def user_invoice():  # USE TABULATE HERE
     invoice_details_lst = [name, phone, email_id, booking_id, source, destination, flight_no, ticket_qty, seat_no, fare]
+    invoice_table = tabulate(invoice_details_lst)
+
     print("""
     ******** INVOICE ********
     Name                : %s
@@ -96,9 +94,10 @@ def user_invoice(): #USE TABULATE HERE
 
     """ % (name, phone, email_id, booking_id, source, destination, flight_no, ticket_qty, seat_no, fare))
 
+
 def save_to_bookings():
     query = "insert into bookings values(%s,'%s','%s','%s','%s','%s','%s','%s','%s','%s',%s,%s,)" % (
-    booking_id, name, email_id, booking_date, flight_date, source, destination, flight_no, seat_no, company_name,
-    ticket_qty, fare)
+        booking_id, name, email_id, booking_date, flight_date, source, destination, flight_no, seat_no, company_name,
+        ticket_qty, fare)
     cursor.execute(query)
     mydb.commit()
