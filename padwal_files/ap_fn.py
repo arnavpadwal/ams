dac4899c307d8b29e95e03a3c1198cd595cf7a25
ArnavRade <116534175+ArnavRade@users.noreply.github.com>
2022-11-06 22:52:23 +0530

    price_calc_locations changed

--- padwal_files/ap_fn.py
+++ padwal_files/ap_fn.py
@@ -9,8 +9,8 @@ cursor = mydb.cursor()
 
 def price_calc():
     global fare
-    price_dict = {"mumbai": 46, "delhi": 50, "kolkata": 60, "chennai": 70, "goa": 45,
-                  "ahmedabad": 38, "pune": 55, "kanpur": 65, "assam": 75, "kerala": 40}
+    price_dict = {"mumbai": 46, "delhi": 50, "kolkata": 60, "chennai": 70, "panji": 45,
+                  "ahmedabad": 38, "pune": 55, "kanpur": 65, "guwahati": 75, "bengaluru": 40}
     fare = price_dict[source.lower()] * price_dict[destination.lower()]
 
 
