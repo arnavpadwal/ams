63259cb167b833e3e0de0e1078a4dc7f06877671
arnavpadwal <arnav.padwal7@gmail.com>
2022-11-10 23:05:08 +0530

    major changes (Data auto entry file)

--- sql-queries/DAE_flights, cabin crew.py
+++ sql-queries/DAE_flights, cabin crew.py
@@ -6,7 +6,7 @@ cursor = mydb.cursor()
 
 fst_names = ["Gage","Gisselle","Javion","Aniya","Keira","Joseph""Brice","Dayanara","Quinn","Camryn","Carly","Armani","Amy","Alondra","Rachel","Kylee","Lily","Diego","Allan","River","Trey","Jillian","Darren"]
 lst_names = ["Beasley","Delgado","Stephenson","Gilbert","Hubbard","Garrett","Farley","Lutz","Pittman","Woods","Leon","Martinez","Hart","Montes","Rodgers","Foley","Nguyen","Hickman","Larsen","Atkins","Miranda","Blair","Nunez","Khan"]
-emp_dict = {"PID":"Pilot", "AID":"Attendant", "LID":"Staff", "SID":"Security", }
+emp_dict = {"PID":"Pilot", "AID":"Attendant"}
 company=["EA","LA","IA","SA"]
 sd=["Mumbai","Delhi","Kolkata","Chennai","Panji","Ahmedabad","Pune","Kanpur","Guwahati","Bengaluru"]
 flt_companies = {"EA": "Emirate", "LA": "Lufthansa", "IA": "Indigo", "SA": "SpiceJet"}
@@ -28,7 +28,7 @@ while i<=500:
     gate_no = random.choice(letters)+str(random.randint(1,3))
 
     while True:
-        name = (random.choice(fst_names))+(random.choice(lst_names))
+        name = (random.choice(fst_names))+" "+(random.choice(lst_names))
         if name not in taken_names:
             taken_names.append(name)
             break
@@ -58,12 +58,8 @@ while i<=500:
     
     
     sql_flt = "insert into flights values('%s','%s','%s','%s')"%(flt_no,company_name,source,dest)
-    sql_staff = "insert into staff values('%s','%s','%s',%s)"%(empid,name,designation,salary)
-    sql_security = "insert into security values('%s','%s','%s',%s)"%(empid,name,gate_no,salary)
     sql_cabin_crew = "insert into cabin_crew values('%s','%s','%s','%s',%s)"%(empid,name,designation,flt_no,salary)
     cursor.execute(sql_flt)
-    cursor.execute(sql_staff)
-    cursor.execute(sql_security)
     cursor.execute(sql_cabin_crew)
     mydb.commit()
     i+=1
