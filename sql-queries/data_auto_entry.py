909f5e9736fdd9c823dab892a0f679c269ac17e7
arnavpadwal <arnav.padwal7@gmail.com>
2022-11-10 22:25:14 +0530

    data auto entry and module changes

--- sql-queries/data_auto_entry.py
+++ sql-queries/data_auto_entry.py
@@ -5,11 +5,12 @@ cursor = mydb.cursor()
 
 
 fst_names = ["Gage","Gisselle","Javion","Aniya","Keira","Joseph""Brice","Dayanara","Quinn","Camryn","Carly","Armani","Amy","Alondra","Rachel","Kylee","Lily","Diego","Allan","River","Trey","Jillian","Darren"]
-lst_name = ["Beasley","Delgado","Stephenson","Gilbert","Hubbard","Garrett","Farley","Lutz","Pittman","Woods","Leon","Martinez","Hart","Montes","Rodgers","Foley","Nguyen","Hickman","Larsen","Atkins","Miranda","Blair","Nunez","Khan"]
+lst_names = ["Beasley","Delgado","Stephenson","Gilbert","Hubbard","Garrett","Farley","Lutz","Pittman","Woods","Leon","Martinez","Hart","Montes","Rodgers","Foley","Nguyen","Hickman","Larsen","Atkins","Miranda","Blair","Nunez","Khan"]
 emp_dict = {"PID":"Pilot", "AID":"Attendant", "LID":"Staff", "SID":"Security", }
 company=["EA","LA","IA","SA"]
 sd=["Mumbai","Delhi","Kolkata","Chennai","Panji","Ahmedabad","Pune","Kanpur","Guwahati","Bengaluru"]
 flt_companies = {"EA": "Emirate", "LA": "Lufthansa", "IA": "Indigo", "SA": "SpiceJet"}
+letters = ["A", "B", "C", "D"]
 first_name = []
 taken_names = []
 taken_flt_no = []
@@ -19,6 +20,13 @@ flt_codes = flt_companies.keys()
 
 i=1
 while i<=500:
+
+    s = random.choice(company)
+    source = random.choice(sd)
+    
+    salary = random.randint(30,100)*1000
+    gate_no = random.choice(letters)+str(random.randint(1,3))
+
     while True:
         name = (random.choice(fst_names))+(random.choice(lst_names))
         if name not in taken_names:
@@ -30,7 +38,10 @@ while i<=500:
             taken_flt_no.append(flt_no)
             break
     while True:
-        empid = random.choice(emp_dict.keys()) + str(random.randint(0001,9997))
+        key= []
+        for k in emp_dict.keys():
+            key.append(k)
+        empid = random.choice(key) + str(random.randint(1111,9997))
         if empid not in taken_empid:
             taken_empid.append(empid)
             break
@@ -42,14 +53,14 @@ while i<=500:
         if j == s:
             company_name = flt_companies[j]
 
-    s = random.choice(company)
-    source = random.choice(sd)
-    designation = emp_dict(empid[0:3])
-    salary = random.randint(30,100)*1000
+    designation = emp_dict[empid[0:3]] 
 
+    
     
     sql_flt = "insert into flights values('%s','%s','%s','%s')"%(flt_no,company_name,source,dest)
     sql_staff = "insert into staff values('%s','%s','%s',%s)"%(empid,name,designation,salary)
+    sql_security = "insert into security values('%s','%s','%s',%s)"%(empid,name,gate_no,salary)
+    sql_cabin_crew = "insert into cabin_crew values('%s','%s','%s','%s',%s)"%(empid,name,designation,flt_no,salary)
     cursor.execute(sql_flt)
     cursor.execute(sql_staff)
     cursor.execute(sql_security)
