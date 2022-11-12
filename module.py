807cfc3b6509152bcdb0832715810f5ac3ee4f33
arnavpadwal <arnav.padwal7@gmail.com>
2022-11-13 00:38:09 +0530

    added time column in flights table

--- module.py
+++ module.py
@@ -1,4 +1,4 @@
-i#DON'T CALL ANY FNs HERE. PLS DELETE FN CALL AFTER TESTING THE FUNCTION.
+#DON'T CALL ANY FNs HERE. PLS DELETE FN CALL AFTER TESTING THE FUNCTION.
 #USE BREAKPOINTS TO TEST FUNCTIONS.
 import mysql.connector
 mydb = mysql.connector.connect(host='localhost', user='root', passwd='root', database='ams')
